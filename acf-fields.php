509e47543472f6b705ecd0eca7c87d77fac97738
Christopher McComas <mccomas.chris@gmail.com>
2026-02-10 15:32:06 -0500

    wip

--- acf-fields.php
+++ acf-fields.php
@@ -601,7 +601,7 @@ acf_add_local_field_group( array(
 			'aria-label' => '',
 			'type' => 'text',
 			'instructions' => '',
-			'required' => 1,
+			'required' => 0,
 			'conditional_logic' => 0,
 			'wrapper' => array(
 				'width' => '',
@@ -660,7 +660,7 @@ acf_add_local_field_group( array(
 			'aria-label' => '',
 			'type' => 'text',
 			'instructions' => '',
-			'required' => 1,
+			'required' => 0,
 			'conditional_logic' => 0,
 			'wrapper' => array(
 				'width' => '',
