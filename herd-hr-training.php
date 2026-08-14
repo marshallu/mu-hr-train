8f20bce6f03cacfa671b01139ee096a89c1d35f2
Christopher McComas <mccomas.chris@gmail.com>
2026-08-14 08:33:14 -0400

    Bump plugin version to 1.1.4

--- herd-hr-training.php
+++ herd-hr-training.php
@@ -9,7 +9,7 @@
  * Plugin Name:  Herd HR Training
  * Plugin URI: https://www.marshall.edu
  * Description: Plugin to allow MU Human Resources to list trainings and allow individuals to register for training.
- * Version: 1.1.3
+ * Version: 1.1.4
  * Author: Christopher McComas
  */
 
